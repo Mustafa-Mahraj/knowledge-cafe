fa7d2d29a0a0e42e8b2b0feebd53da1a0c871dd4
unknown <mustafamahraj4002@gmail.com>
2024-10-28 00:40:35 +0600

    setup tailwind in react JS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl bg-red-200 text-center p-4 mt-2'>knowledge-cafe</h1>
+
+    </>
+  )
+}
+
+export default App
