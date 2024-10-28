64194ff87a69aeb0e3b1c1e9c861f0136a97e086
unknown <mustafamahraj4002@gmail.com>
2024-10-28 16:12:17 +0600

    create some fake JSON data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import Blogs from './components/blogs/Blogs'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl bg-red-200 text-center p-4 mt-2'>knowledge-cafe</h1>
-
+    <Header></Header>
+    <Blogs></Blogs>
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl bg-red-200 text-center p-4 mt-2'>knowledge-cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
